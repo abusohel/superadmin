24c9d66e29990603f394c874e643d85f6ce45320
abu sohel <abusoheljnu@gmail.com>
2018-03-14 17:49:42 +0600

    5.5 with auth

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Admin Dashboard</div>
  
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
  
-                     You are logged in!
+                    This is Admin Dashboard. You must be privileged to be here !
                 </div>
             </div>
         </div>
